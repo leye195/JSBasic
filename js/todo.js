3b4d0b68add359b2e29ed976922d68660745ae8c
leye195 <leye195@naver.com>
2019-11-27 20:22:59 +0900

    todo doing done commit

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,6 @@
 const toDoForm =document.querySelector(".memo-toDoForm"),
 toDoInput=toDoForm.querySelector("input"),toDoList=document.querySelector(".memo-toDoList");
-toDoneList=document.querySelector('.memo-doneList');
+toDoneList=document.querySelector('.memo-doneList'),toDoingList=document.querySelector('.memo-doingList');
 let toDos=[];
 let doingList=[];
 let doneList=[];
@@ -35,21 +35,25 @@ function printDoing(text){
     const del=document.createElement("button");
     const done=document.createElement("button");
     const date_id=new Date().getTime();
-
     del.innerText="❌";
     del.id="del";
-    del.addEventListener("click",handleDeleteDone);
+    done.innerText="✅";
+    done.id="done";
+    del.addEventListener("click",handleDeleteDoing);
+    done.addEventListener("click",handleDone);
+
     const span=document.createElement("span");
     span.innerText=text;
     li.appendChild(span);
     li.appendChild(del);
+    li.appendChild(done);
     li.id=date_id
-    toDoneList.appendChild(li);
+    toDoingList.appendChild(li);
     const doneObj={
         text:text,
         id:date_id
     }
-    doneList.push(doneObj);
+    doingList.push(doneObj);
     saveDoing();
 }
 function printDone(text){
@@ -57,7 +61,6 @@ function printDone(text){
     const del=document.createElement("button");
     const done=document.createElement("button");
     const date_id=new Date().getTime();
-
     del.innerText="❌";
     del.id="del";
     del.addEventListener("click",handleDeleteDone);
@@ -92,18 +95,31 @@ function handleSubmit(e){
         toDoInput.value="";
     }
 }
-function handleDone(e){
+function handleDoing(e){
     const btn=e.target;
     const li = btn.parentNode;
     const ul = document.querySelector(".memo-toDoList");
     ul.removeChild(li);
     const cleanDo=toDos.filter((item)=>item.id!==parseInt(li.id));
-    const cleanDone=toDos.filter((item)=>item.id===parseInt(li.id));
+    const cleanDoing=toDos.filter((item)=>item.id===parseInt(li.id));
     toDos=cleanDo;
 
     saveToDos();
+    printDoing(cleanDoing[0].text);
+}
+function handleDone(e){
+    const btn=e.target;
+    const li = btn.parentNode;
+    const ul = document.querySelector(".memo-doingList");
+    ul.removeChild(li);
+    const cleanDoing=doingList.filter((item)=>item.id!==parseInt(li.id));
+    const cleanDone=doingList.filter((item)=>item.id===parseInt(li.id));
+    doingList=cleanDoing;
+
+    saveDoing();
     printDone(cleanDone[0].text);
 } 
+
 function handleDeleteToDo(e){
     const btn=e.target;
     const li = btn.parentNode;
@@ -113,6 +129,17 @@ function handleDeleteToDo(e){
     const cleanToDo=toDos.filter((item)=>item.id!==parseInt(li.id))
     toDos=cleanToDo; //replace toDos = cleanTodo
     saveToDos();  //then save ToDos
+}
+function handleDeleteDoing(e){
+    const btn=e.target;
+    const li = btn.parentNode;
+    const ul = document.querySelector(".memo-doingList");
+    ul.removeChild(li);
+    console.log(li.id);
+    const cleanDone=doneList.filter((item)=>item.id!==parseInt(li.id))
+    console.log(cleanDone);
+    doneList=cleanDone; //replace toDos = cleanTodo
+    saveDoing();  //then save ToDos
 }
 function handleDeleteDone(e){
     const btn=e.target;
@@ -127,6 +154,7 @@ function handleDeleteDone(e){
 }
 function loadToDo(){
     const toDo=localStorage.toDo;
+    const doing=localStorage.doing;
     const done=localStorage.done;
     console.log(toDo);
     if(toDo){
@@ -134,6 +162,12 @@ function loadToDo(){
         loaded.forEach(element => {
             printTodo(element.text);
         });
+    }
+    if(doing){
+        const loaded=JSON.parse(doing);
+        loaded.forEach(element=>{
+            printDoing(element.text);
+        })
     }
     if(done){
         const loaded=JSON.parse(done);
